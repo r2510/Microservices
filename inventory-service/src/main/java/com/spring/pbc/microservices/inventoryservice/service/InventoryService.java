4b08e9cb8f9cb7fb397a06226b9aa72e59b169c7
Rahul Billava <rahulbillava103@gmail.com>
2023-07-19 22:42:17 +0530

    Trace

--- inventory-service/src/main/java/com/spring/pbc/microservices/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/spring/pbc/microservices/inventoryservice/service/InventoryService.java
@@ -4,6 +4,8 @@ import com.spring.pbc.microservices.inventoryservice.dto.InventoryResponse;
 import com.spring.pbc.microservices.inventoryservice.model.Inventory;
 import com.spring.pbc.microservices.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows//not recomended
     public List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Waait Started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder().skuCode(inventory.getSkuCode())
